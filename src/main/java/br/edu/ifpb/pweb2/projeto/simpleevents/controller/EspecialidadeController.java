1aaf147720606d712068d6c1072cc533e130ab19
Pah Ferreira <pah.dsf@gmail.com>
2019-05-29 16:46:46 -0300

    Especialidades Controller

--- src/main/java/br/edu/ifpb/pweb2/projeto/simpleevents/controller/EspecialidadeController.java
+++ src/main/java/br/edu/ifpb/pweb2/projeto/simpleevents/controller/EspecialidadeController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/events")
-public class EventController {
+@RequestMapping("/especialidades")
+public class EspecialidadeController {
 
   @RequestMapping(method = RequestMethod.GET)
   public String home() {
-    return "events";
+    return "especialidades-form";
   }
 }
