5168342d51b7e51be6399fb5ffa5a2e25bc42a01
Pah Ferreira <pah.webdev@gmail.com>
2019-06-04 11:49:21 -0300

    updating event

--- src/main/java/br/edu/ifpb/pweb2/projeto/simpleevents/model/Evento.java
+++ src/main/java/br/edu/ifpb/pweb2/projeto/simpleevents/model/Evento.java
@@ -24,7 +24,7 @@ import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
-@Table(name="TB_EVENTO")
+@Table(name = "TB_EVENTO")
 @Getter
 @Setter
 @ToString
@@ -32,31 +32,34 @@ public class Evento {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@NotEmpty(message="Campo descrição não deve estar vazio")
+
+	@NotEmpty(message = "Campo descrição não deve estar vazio")
 	private String descricao;
-	
-	@NotNull(message="Campo data é obrigatório")
+
+	@NotNull(message = "Campo data é obrigatório")
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-	@Future(message="O evento deverá ocorrer em uma data futura")
+	@Future(message = "O evento deverá ocorrer em uma data futura")
 	private LocalDateTime data;
-	@NotNull(message="Campo data é obrigatório")
+	@NotNull(message = "Campo data é obrigatório")
 	private String nome;
 	private String local;
 	private boolean isFinalizado;
 
 	@ManyToOne
 	private Usuario dono;
-	
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "evento", cascade = CascadeType.ALL)
-    @ElementCollection
-	private List<Vaga> vagas = new ArrayList<>();
-	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy="evento", cascade = CascadeType.ALL)
-    private List<Avaliacao> avaliacao = new ArrayList<>();
-    
-    public void addVaga(Vaga vaga) {
-        this.vagas.add(vaga);
-    }
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "evento", cascade = CascadeType.ALL)
+	@ElementCollection
+	private List<Vaga> vagas = new ArrayList<>();
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "evento", cascade = CascadeType.ALL)
+	private List<Avaliacao> avaliacao = new ArrayList<>();
+
+	public void addVaga(Vaga vaga) {
+		this.vagas.add(vaga);
+	}
+
+	public void removeVaga(Vaga vaga) {
+		this.vagas.remove(vaga);
+	}
 }
